1676be4cdd308a5e91e275566e21489787a53cf8
joseph achonu <josephachonu@gmail.com>
2022-04-06 00:16:20 +0100

    initial commit

---
+++ scripts/updateContract.js
@@ -0,0 +1,36 @@
+const asyncfs = require("fs/promises");
+const fs = require("fs");
+const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config();
+
+async function updateClientContract() {
+  try {
+    let contractName = process.env.contractNames;
+    await deleteFiles();
+    contractName.split(",").forEach(async (element) => {
+      console.log(element);
+      await asyncfs.copyFile(
+        `artifacts/contracts/${element}.sol/${element}.json`,
+        `client/contracts/${element}.json`
+      );
+    });
+
+    console.log("Solidity contracts was successfully copied to client.");
+    process.exit(0);
+  } catch (e) {
+    console.log(e);
+    process.exit(1);
+  }
+}
+
+async function deleteFiles() {
+  const directory = "client/contracts";
+  await asyncfs.rmdir(directory, {
+    recursive: true,
+  });
+
+  asyncfs.mkdir("client/contracts");
+}
+
+updateClientContract();
