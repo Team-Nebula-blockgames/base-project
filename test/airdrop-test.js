5d8d7841a90a9064581aad7d588d7ba013e71c5c
SanaUdoekong <udoekongsana@gmail.com>
2022-04-06 17:55:11 +0100

    test

--- test/airdrop-test.js
+++ test/airdrop-test.js
@@ -1,52 +1,43 @@
 const { ethers } = require("hardhat");
 const { use, expect } = require("chai");
 const { solidity } = require("ethereum-waffle");
-const { accounts, contract } = require('@openzeppelin/test-environment');
+const { ContractFactory } = require("ethers");
 
 use(solidity);
 
-describe("NestCoinFilmHouse", function () {
-  let multiTransferTokenEqual;
+describe("MultiTransferTokenEqual", function () {
 
-  // quick fix to let gas reporter fetch data from gas station & coinmarketcap
-  before((done) => {
-    setTimeout(done, 2000);
-  });
 
-  describe("MultiTransferTokenEqual", function () {
-    it("Should deploy token contract", async function() 
-    {
-        // accounts[0] -> Contract Owner
-        const Nestcoin = await ethers.getContractFactory("Nestcoin");
-        nestCoin = await Nestcoin.deploy();
-    });
-   
-    it("Should deploy MultiTransferTokenEqual", async function () {
-      const Nestcoin = await ethers.getContractFactory("Nestcoin");
-      const MultiTransferTokenEqual = await ethers.getContractFactory("MultiTransferTokenEqual");
-      nestCoin = await Nestcoin.deploy();
-      multiTransferTokenEqual = await MultiTransferTokenEqual.deploy();
-    });
+beforeEach(async() => {
+    accounts = await ethers.getSigners();
+    owner = accounts[0];
+    contractFactory = await ethers.getContractFactory("MultiTransferTokenEqual");
+    Nestcoin = await ethers.getContractFactory("Nestcoin");
+    MultiTransferTokenEqual = await ethers.getContractFactory("MultiTransferTokenEqual");
+    nestCoin = await Nestcoin.deploy();
+    multiTransferTokenEqual = await MultiTransferTokenEqual.deploy();
+    await nestCoin.deployed()
+    await multiTransferTokenEqual.deployed()
+});
 
-    it("Should perform a batch transfer", async function () {
-        let _addresses = [];
-        let amount = 1;
-        let _token = "0x0f130701Dfe335278f1203e134ACe84d655f1b25";
-        const Nestcoin = await ethers.getContractFactory("Nestcoin");
-        const MultiTransferTokenEqual = await ethers.getContractFactory("MultiTransferTokenEqual");
-        nestCoin = await Nestcoin.deploy();
-        multiTransferTokenEqual = await MultiTransferTokenEqual.deploy();
+it("Should deploy token contract", async function() {
+    expect(await nestCoin.deployed(), "Token contract not deployed");
+});
 
-        for(var i=0; i<_addresses.length; i++) {
-          await multiTransferTokenEqual.multiSend(_token, _addresses[i], amount)
-        }
+it("Should deploy MultiTransferTokenEqual", async function () { 
+    expect(await multiTransferTokenEqual.deployed(), "MultiTransferTokenEqual not deployed");
+});
 
-        
-        let contractTokenBalance = (await nestCoin.balanceOf(multiTransferTokenEqual.address)).toNumber();
-        console.log("Transferring tokens to ", _addresses.length, " account(s)");
-        expect ((await nestCoin.balanceOf(nestCoin.address)).toNumber()).to.be.greaterThanOrEqual(amount, "Token balance is low, mint more tokens!");
+it("Should perform a batch transfer", async function () {
+    let amount = 0;
+    let _token = nestCoin.address;
+    let contractTokenBalance = (await nestCoin.balanceOf(multiTransferTokenEqual.address)).toNumber();
 
-        contractTokenBalance = (await nestCoin.balanceOf(multiTransferTokenEqual.address)).toNumber();
-    });
-  });
+    
+    await multiTransferTokenEqual.multiSend(_token, accounts, amount);
+    console.log("Transferring tokens to ", accounts.length, " account(s)");
+    expect ((await nestCoin.balanceOf(owner.address)).toNumber()).to.be.greaterThanOrEqual(amount, "Token balance is low, mint more tokens!");
+
+    contractTokenBalance = (await nestCoin.balanceOf(multiTransferTokenEqual.address)).toNumber();
+});
 });
