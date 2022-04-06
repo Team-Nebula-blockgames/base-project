22e1a631721c1a03b99a4d890d4fa02c0ed681d0
SanaUdoekong <udoekongsana@gmail.com>
2022-04-06 19:09:36 +0100

    airdrop test

--- test/airdrop-test.js
+++ test/airdrop-test.js
@@ -17,7 +17,8 @@ beforeEach(async() => {
     multiTransferTokenEqual = await MultiTransferTokenEqual.deploy();
     await nestCoin.deployed();
     await multiTransferTokenEqual.deployed();
-    owner = accounts[0];
+    ownerNestcoin = nestCoin.address;
+    ownerMultiTransferTokenEqual = multiTransferTokenEqual.address;
 });
 
 it("Should deploy token contract", async function() {
@@ -30,15 +31,15 @@ it("Should deploy MultiTransferTokenEqual", async function () {
 
 it("Should perform a batch transfer", async function () {
     let amount = 0;
-    let _addresses = [accounts[2].address, accounts[3].address, accounts[4].address ];
-    let _token = nestCoin.address;
-    let contractTokenBalance = (await nestCoin.balanceOf(owner)).toNumber();
+    let _addresses = [accounts[2].address, accounts[3].address, accounts[4].address];
+    let _token = ownerNestcoin;
+    let contractTokenBalance = (await nestCoin.balanceOf(ownerMultiTransferTokenEqual)).toNumber();
 
     
     await multiTransferTokenEqual.multiSend(_token, _addresses, amount);
     console.log("Transferring tokens to ", _addresses.length, " account(s)");
-    expect ((await nestCoin.balanceOf(owner)).toNumber()).to.be.greaterThanOrEqual(amount, "Token balance is low, mint more tokens!");
+    expect ((await nestCoin.balanceOf(ownerNestcoin)).toNumber()).to.be.greaterThanOrEqual(amount, "Token balance is low, mint more tokens!");
 
-    contractTokenBalance = (await nestCoin.balanceOf(owner)).toNumber();
+    contractTokenBalance = (await nestCoin.balanceOf(ownerMultiTransferTokenEqual)).toNumber();
 });
 });
