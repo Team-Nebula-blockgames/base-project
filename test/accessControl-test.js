0a8519d26f88f81f3086cd9c8c5911dfa3e5390c
SanaUdoekong <udoekongsana@gmail.com>
2022-04-08 20:47:47 +0100

    modified accessControl-test

--- test/accessControl-test.js
+++ test/accessControl-test.js
@@ -9,11 +9,28 @@ describe("AccessControl", function () {
 
     beforeEach(async() => {
         accounts = await ethers.getSigners();
-        contractFactory = await ethers.getContractFactory("AccessControl");
-        AccessControl = await ethers.getContractFactory("AccessControl");
-        accessControl = await AccessControl.deploy();
+
+        accessControlFactory = await ethers.getContractFactory("AccessControl");
+        accessControl = await accessControlFactory.deploy();
         await accessControl.deployed();
-        ADMIN = "0xdf8b4c520ffe197c5343c6f5aec59570151ef9a492f2c624fd45ddde6135ec42";
+
+        let nestCoinFactory = await ethers.getContractFactory("Nestcoin");
+        nestCoinContract = await nestCoinFactory.deploy();
+        await nestCoinContract.deployed();
+    
+        let multiTransferTokenEqualFactory = await ethers.getContractFactory(
+          "MultiTransferTokenEqual"
+        );
+        multiTransferTokenEqualContract =
+          await multiTransferTokenEqualFactory.deploy();
+        await multiTransferTokenEqualContract.deployed();
+
+
+        newAdmin = accounts[1];
+        ADMIN_HASH =
+          "0xdf8b4c520ffe197c5343c6f5aec59570151ef9a492f2c624fd45ddde6135ec42"; //hash for role admin
+        await nestCoinContract.grantRole(ADMIN_HASH, newAdmin.address);   
+
         owner = accounts[0].address;
     });
 
@@ -22,23 +39,67 @@ describe("AccessControl", function () {
     });
 
     it("Admin should be able to transfer ownership", async function() {
-        let _role = ADMIN;
+        let _role = ADMIN_HASH;
         let _account = accounts[3].address;
-        // expect(amount).to.be.greaterThan(0, "You cannot mint zero tokens!");
-        console.log("Granting...");
         await accessControl.grantRole(_role, _account);
-        // expect((await nestCoin.balanceOf(owner)).toNumber()).to.be.greaterThanOrEqual(amount, "Tokens were not successfully minted!");
         
     });
 
     it("Admin should be able to revoke ownership", async function() {
-        let _role = ADMIN;
+        let _role = ADMIN_HASH;
         let _account = accounts[3].address;
-        // expect(amount).to.be.greaterThan(0, "You cannot mint zero tokens!");
-        console.log("Revoking...");
         await accessControl.grantRole(_role, _account);
-        // expect((await nestCoin.balanceOf(owner)).toNumber()).to.be.greaterThanOrEqual(amount, "Tokens were not successfully minted!");
-        
     });
+
+    
+    it("Should allow new admins mint tokens.", async () => {
+        let amount = 10;
+        let mintedAmount = (10 * 10 ** 18).toString();
+        
+        await nestCoinContract.connect(newAdmin).mint(amount);
+        expect(await nestCoinContract.balanceOf(newAdmin.address)).to.equals(mintedAmount, "Couldn't mint tokens!");
+    });
+
+    it("Should allow new admins perform batch token transfers.", async () => {
+    
+        let amount = 10;
+        let mintedTokens = 1000;
+        let _addresses = [
+          accounts[2].address,
+          accounts[3].address,
+          accounts[4].address,
+        ];
+    
+        await nestCoinContract.connect(newAdmin).mint(mintedTokens);
+        await nestCoinContract
+          .connect(newAdmin)
+          .approve(
+            multiTransferTokenEqualContract.address,
+            ethers.utils.parseEther(String(mintedTokens))
+          );
+        let newAdminBalance = Number(
+          await nestCoinContract.balanceOf(newAdmin.address)
+        );
+    
+        await multiTransferTokenEqualContract
+          .connect(newAdmin)
+          .multiSend(nestCoinContract.address, _addresses, amount);
+    
+        expect(Number(await nestCoinContract.balanceOf(newAdmin.address))).equals(
+          newAdminBalance -
+            Number(ethers.utils.parseEther(String(amount)) * _addresses.length)
+        );
+    });
+
+    it("New admin should be able to transfer ownership", async function() {
+      let _account = accounts[3].address;
+      await accessControl.grantRole(ADMIN_HASH, _account);
+      
+  });
+
+  it("New admin should be able to revoke ownership", async function() {
+      let _account = accounts[3].address;
+      await accessControl.grantRole(ADMIN_HASH, _account);
+  });
 
 });
--- test/nestcoin-test.js
+++ test/nestcoin-test.js
@@ -25,10 +25,9 @@ describe("Token", function () {
     it("Admin should be able to mint tokens", async function() {
         let amount = 10;
         let mintedAmount = (10 * 10 ** 18).toString();
-        console.log("Minting...");
         await nestCoin.mint(amount);
         expect(await nestCoin.balanceOf(owner)).to.equals(mintedAmount, "Tokens were not successfully minted!");
-        console.log("Minted ", amount," tokens to owner successfully!");
+       
     });
 
     it("Owner should be able to destroy contract", async function() {
