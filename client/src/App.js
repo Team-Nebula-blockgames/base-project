2dcd8cbce513e406366e1ff7b2ef3aae8b7ca863
Oyewande Oluwaseun <seunemmanuel2255@gmail.com>
2022-04-09 14:43:40 +0100

    Completed new changes to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import getEthers from "./getEthers";
 import { Contract, utils } from "ethers";
 import Token from "./contracts/Nestcoin.json";
 import Distributor from "./contracts/MultiTransferTokenEqual.json";
-import Control from "./contracts/AccessControl.json";
 import "./App.css";
 import Box from "@mui/material/Box";
 import NavBar from "./components/navbar";
@@ -19,12 +18,11 @@ function App() {
   const [tokens, setTokens] = useState(0);
   const [distributorMethods, setDistributorMethods] = useState({});
   const [tokenMethods, setTokenMethods] = useState({});
-  const [controlMethods, setControlMethods] = useState({});
   const [view, setView] = useState("admin");
   const [tokenCheck, setTokenCheck] = useState();
   const [modal, setModal] = useState(false);
-  console.log(process.env);
-  console.log(process.env.REACT_APP_TOKEN_ADDRESS);
+  const [ticketsBalance, setTicketsBalance] = useState(0);
+  const [ticket_price, setTicket_price] = useState(0);
 
   useEffect(() => {
     const getData = async () => {
@@ -41,25 +39,16 @@ function App() {
         Distributor.abi,
         provider
       );
-      const controlContract = new Contract(
-        process.env.REACT_APP_CONTROL_ADDRESS,
-        Control.abi,
-        provider
-      );
       setDistributorMethods(distributorContract.connect(signer));
       setTokenMethods(tokenContract.connect(signer));
-      setControlMethods(controlContract.connect(signer));
       // getting the balance of tokens and setting it
       const tokenBalance = (await tokenContract.balanceOf(address)).toString();
       setTokenCheck(tokenContract);
       setTokens(utils.formatEther(tokenBalance));
-      // contract.on("Transfer", async (from, to, amount, event) => {
-      //   const bal = await provider.getBalance(address);
-      //   const balance = utils.formatEther(bal);
-      //   const tokenBalance = (await contract.balanceOf(address)).toString();
-      //   setBalance(tokenBalance);
-      //   setEthBalance(balance);
-      // });
+      const ticketBal = await distributorContract.ticket(address);
+      setTicketsBalance(ticketBal.toString());
+      const price = await distributorContract.ticketPrice();
+      setTicket_price(utils.formatEther(price));
     };
     getData().then(() => {});
   }, []);
@@ -86,9 +75,13 @@ function App() {
         {view === "systemhealth" && <SystemHealth methods={tokenMethods} />}
         {view === "buytickets" && (
           <BuyTickets
-            methods={tokenMethods}
             setTokens={setTokens}
             tokens={tokens}
+            tokenMethod={tokenMethods}
+            distributorMethod={distributorMethods}
+            ticketsBalance={ticketsBalance}
+            setTicketsBalance={setTicketsBalance}
+            ticket_price={ticket_price}
           />
         )}
       </Box>
--- client/src/components/buyTickets.js
+++ client/src/components/buyTickets.js
@@ -2,37 +2,47 @@ import React, { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import ButtonGroup from "@mui/material/ButtonGroup";
 import { Contract, utils } from "ethers";
-import AddSharp from "@material-ui/icons/AddSharp";
-import Remove from "@material-ui/icons/Remove";
 import Token from "../contracts/Nestcoin.json";
 import getEthers from "../getEthers";
 
 function BuyTickets(props) {
-  const { tokens, ticketPrice } = props;
+  const {
+    tokens,
+    setTokens,
+    ticketsBalance,
+    setTicketsBalance,
+    tokenMethod,
+    distributorMethod,
+    ticket_price,
+  } = props;
   const [balance, setBalance] = useState(tokens);
-  const [tickets, setTickets] = useState(0);
+  const [tickets, setTickets] = useState(ticketsBalance);
+  const [open, setOpen] = useState(false);
+  const [open2, setOpen2] = useState(false);
+  const [ticketPrice, setTicketPrice] = useState(ticket_price);
+  console.log("Hello");
 
   useEffect(() => {
     const getData = async () => {
       const provider = await getEthers();
       const signer = provider.getSigner();
       const address = signer.getAddress();
-      const tokenContract = new Contract(
-        process.env.REACT_APP_TOKEN_ADDRESS,
+      const distributorContract = new Contract(
+        process.env.REACT_APP_DISTRIBUTOR_ADDRESS,
         Token.abi,
         provider
       );
-
-      tokenContract.balanceOf(address).then((result) => {
-        console.log("Its updating");
-        setBalance(utils.formatEther(result));
-      });
+      const ticketBal = await distributorContract.ticket(address);
+      const price = await distributorContract.ticketPrice();
+      setTickets(ticketBal.toString());
+      setTicketPrice(utils.formatEther(price));
     };
 
-    getData().then(() => {});
-  }, [tokens]);
+    getData().then(() => {
+      console.log("Worked?");
+    });
+  }, []);
 
   return (
     <Box
@@ -47,6 +57,17 @@ function BuyTickets(props) {
         position: "relative",
       }}
     >
+      <Typography
+        sx={{
+          position: "absolute",
+          top: "20px",
+          left: "20px",
+          fontWeight: 700,
+          fontSize: "20px",
+        }}
+      >
+        Tickets: {tickets}
+      </Typography>
       <Typography
         sx={{
           position: "absolute",
@@ -92,143 +113,104 @@ function BuyTickets(props) {
         >
           One ticket is worth {ticketPrice ? "10" : "50"} NCT
         </Typography>
-        <ButtonGroup size="small">
-          <Button
-            startIcon={
-              <Remove style={{ color: "#8a2be2", backgroundColor: "white" }} />
-            }
-            variant="contained"
-            style={{
-              backgroundColor: "white",
-              color: "#8a2be2",
-            }}
-            onClick={() => setTickets(tickets - 1)}
-          >
-            Minus
-          </Button>
-          <Button
-            startIcon={
-              <AddSharp
-                style={{ color: "#8a2be2", backgroundColor: "white" }}
-              />
-            }
-            variant="contained"
-            style={{
-              backgroundColor: "white",
-              color: "#8a2be2",
-            }}
-            onClick={() => setTickets(tickets + 1)}
-          >
-            Plus
-          </Button>
-        </ButtonGroup>
-        <div style={style.tokenDisplay}>
-          <p style={style.text}>{tickets}</p>
-        </div>
-        {balance > 0 && (
-          <Box sx={{ padding: 0, marginTop: "10px" }}>
-            <Typography
-              sx={{
-                fontWeight: 700,
-                fontSize: "14px",
-                lineHeight: "17px",
-                color: "#000000",
-                opacity: 0.5,
-              }}
-            >
-              Your buying {tickets} tickets, you'll pay {tickets * ticketPrice}{" "}
-              NCT
-            </Typography>
-            <Button
-              variant="contained"
-              sx={{
-                width: "120px",
-                height: "60px",
-                background: "#1949D9",
-                borderRadius: "8px",
-                color: "white",
-                fontWeight: 700,
-                fontSize: "20px",
-                lineHeight: "24px",
-                marginLeft: "15px",
-                marginTop: "10px",
-              }}
-              onClick={() => {}}
-            >
-              Buy
-            </Button>
-          </Box>
-        )}
+        <Button
+          variant="contained"
+          sx={{
+            width: "120px",
+            height: "60px",
+            background: "#1949D9",
+            borderRadius: "8px",
+            color: "white",
+            fontWeight: 700,
+            fontSize: "20px",
+            lineHeight: "24px",
+            marginLeft: "15px",
+            marginTop: "20px",
+          }}
+          onClick={() => {
+            tokenMethod
+              .approve(
+                process.env.REACT_APP_DISTRIBUTOR_ADDRESS,
+                utils.parseEther("50")
+              )
+              .then((result) => {
+                if (result)
+                  distributorMethod
+                    .claimTicket(process.env.REACT_APP_TOKEN_ADDRESS)
+                    .then(() => {
+                      setTickets(Number(tickets) + 1);
+                      setTicketsBalance(Number(tickets) + 1);
+                      setTokens(tokens - ticketPrice);
+                      setBalance(balance - ticketPrice);
+                      setOpen(true);
+                      setTimeout(() => {
+                        setOpen(false);
+                      }, 1500);
+                    })
+                    .catch(() => {
+                      setOpen2(true);
+                      setTimeout(() => {
+                        setOpen2(false);
+                      }, 1500);
+                    });
+              });
+          }}
+        >
+          Buy
+        </Button>
       </Box>
+      {open && (
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            zIndex: 20,
+            background: "green",
+            color: "white",
+            width: "500px",
+            height: "100px",
+            position: "absolute",
+          }}
+        >
+          <Typography
+            sx={{
+              color: "white",
+              fontSize: "24px",
+              fontWeight: 700,
+            }}
+          >
+            You bought one Ticket!
+          </Typography>
+        </Box>
+      )}
+      {open2 && (
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            zIndex: 20,
+            background: "red",
+            color: "white",
+            width: "500px",
+            height: "100px",
+            position: "absolute",
+          }}
+        >
+          <Typography
+            sx={{
+              color: "white",
+              fontSize: "24px",
+              fontWeight: 700,
+            }}
+          >
+            Transaction Failed
+          </Typography>
+        </Box>
+      )}
     </Box>
   );
 }
-
-const style = {
-  outer: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: "8px 5px 8px 8px",
-
-    position: "relative",
-    width: "461px",
-    height: "60px",
-
-    border: "1px solid #C4C4C4",
-    boxSizing: "border-box",
-    borderRadius: "8px",
-  },
-
-  tokenDisplay: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    width: "40px",
-    height: "40px",
-
-    border: "1px solid #C4C4C4",
-    boxSizing: "border-box",
-    borderRadius: "8px",
-    marginTop: "10px",
-  },
-
-  text: {
-    fontStyle: "normal",
-    fontWeight: 400,
-    fontSize: "20px",
-    lineHeight: "24px",
-    color: "blue",
-  },
-
-  imgContainer2: {
-    width: "66px",
-    height: "39px",
-    cursor: "pointer",
-    position: "absolute",
-    left: "390px",
-    top: "10.5px",
-  },
-
-  image: {
-    width: "100%",
-    height: "100%",
-  },
-
-  input2: {
-    width: "80%",
-    height: "100%",
-    display: "flex",
-    alignItems: "center",
-    border: "none",
-    outline: "none",
-    fontStyle: "normal",
-    fontWeight: 400,
-    fontSize: "20px",
-    lineHeight: "24px",
-    color: "#938989",
-  },
-};
 
 export default BuyTickets;
